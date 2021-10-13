069dfda7b37bbb01b6d75e6b38e0bcbc31e34f87
Doctor <thirvondukr@gmail.com>
2021-10-13 12:31:15 +0300

    Fix import of asyncontextmanager from wrong package

--- servers/fastapi-sqlalchemy/db/base.py
+++ servers/fastapi-sqlalchemy/db/base.py
@@ -1,6 +1,7 @@
+from contextlib import asynccontextmanager
+
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy.util.concurrency import asynccontextmanager
 
 engine = create_async_engine(
     "sqlite+aiosqlite:///db.sqlite3",
